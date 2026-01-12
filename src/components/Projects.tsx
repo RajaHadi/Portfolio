5abb8094b35b2174f6c3fce79b91674c801eb11c
Raja Mannan Khan <shabanajawed411@gmail.com>
2026-01-12 09:58:57 +0500

    projects-Updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Projects = () => {
-  const categories = ["All", "Web Development", "E-commerce"];
+  const categories = ["All", "Web Development", "E-commerce", "AI-Powered"];
   const [activeCategory, setActiveCategory] = useState("All");
 
   const projects = [
@@ -13,62 +13,81 @@ const Projects = () => {
       category: "Web Development",
       image: "/projects/Raja.png",
       description: "This is Governor's IT Initiative Website I Cloned.",
-      link: "https://github.com/RajaMannan/governor-website-clone"
+      link: "https://github.com/RajaMannan/governor-website-clone",
     },
     {
       title: "Blog Website",
       category: "Web Development",
       image: "/projects/Blog_App.png",
       description: "A simple blog website with basic functionality.",
-      link: "https://blog-website-flax-eight.vercel.app/"
+      link: "https://blog-website-flax-eight.vercel.app/",
     },
     {
       title: "Gentleman Clothing",
       category: "E-commerce",
       image: "/projects/Gentleman.png",
-      description: "Modern e-commerce clothing store for men built with Next.js 15.",
-      link: "https://gentleman-shop.vercel.app/"
+      description:
+        "Modern e-commerce clothing store for men built with Next.js 15.",
+      link: "https://gentleman-shop.vercel.app/",
     },
     {
       title: "Restaurant Website",
       category: "Web Development",
       image: "/projects/Food.png",
       description: "A Food Restaurant Website I developed For My UI Hackathon.",
-      link: "https://my-hackathon-flame.vercel.app/"
+      link: "https://my-hackathon-flame.vercel.app/",
     },
     {
       title: "Raja's Burger Point",
       category: "Web Development",
       image: "/projects/Raja_Burger.png",
-      description: "A simple and attractive front-end created for Raja's Burgers.",
-      link: "https://landing-page-fawn-xi-14.vercel.app/"
+      description:
+        "A simple and attractive front-end created for Raja's Burgers.",
+      link: "https://landing-page-fawn-xi-14.vercel.app/",
     },
     {
       title: "Luxe Bags",
       category: "E-commerce",
       image: "/projects/LuxeBags.png",
       description: "A attractive and resposive woman bags and purses store.",
-      link: "https://v0-handbag-website-design-black.vercel.app/"
+      link: "https://v0-handbag-website-design-black.vercel.app/",
     },
     {
       title: "Humonoid Robotics Book",
       category: "Web Development",
       image: "/projects/Book.png",
-      description: "Master humanoid control, embodied intelligence, and VLA systems through our comprehensive engineering guide..",
-      link: "https://rajahadi.github.io/-physical-ai-humanoid-robotics-book/"
+      description:
+        "Master humanoid control, embodied intelligence, and VLA systems through our comprehensive engineering guide..",
+      link: "https://rajahadi.github.io/-physical-ai-humanoid-robotics-book/",
+    },
+    {
+      title: "File Transformer & Data Cleaner",
+      category: "AI-Powered",
+      image: "/projects/Growth.png",
+      description:
+        "Upload your CSV or Excel files to clean, visualize, and convert them.",
+      link: "https://growth-mindset-challenge-bbpdjcy3zw2s42nzszgzjq.streamlit.app/",
+    },
+    {
+      title: "Crypto-Assistant-Agent",
+      category: "AI-Powered",
+      image: "/projects/crypto.png",
+      description: "",
+      link: "https://crypto-assistant-agent-umrzr6lqrfeadd7jjbb7gp.streamlit.app/",
     },
   ];
 
-  const filteredProjects = activeCategory === "All" 
-    ? projects 
-    : projects.filter(p => p.category === activeCategory);
+  const filteredProjects =
+    activeCategory === "All"
+      ? projects
+      : projects.filter((p) => p.category === activeCategory);
 
   return (
     <section id="projects" className="px-8 py-20 max-w-7xl mx-auto">
       <div className="text-center mb-12 space-y-4">
         <h2 className="text-4xl font-bold text-white">My Projects</h2>
         <p className="text-gray-400 max-w-2xl mx-auto">
-          A collection of projects I’ve built with Next.js, Tailwind, and modern web technologies.
+          A collection of projects I’ve built with modern web technologies.
         </p>
       </div>
 
@@ -90,21 +109,28 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {filteredProjects.map((item, index) => (
-          <div key={index} className="group relative overflow-hidden rounded-2xl aspect-[4/3] bg-[#2a2a2a]">
+          <div
+            key={index}
+            className="group relative overflow-hidden rounded-2xl aspect-[4/3] bg-[#2a2a2a]"
+          >
             <div className="absolute inset-0 bg-[#2a2a2a] flex items-center justify-center text-gray-600 group-hover:scale-105 transition-transform duration-500">
-                <Image 
-                  src={item.image} 
-                  alt={item.title}
-                  fill
-                  className="object-cover"
-                />
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-cover"
+              />
             </div>
             <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition-opacity flex flex-col items-center justify-end pb-8 gap-2 p-6 text-center">
-                <h3 className="text-md font-bold text-primary">{item.category}</h3>
-                <p className="text-white font-bold text-xl">{item.title}</p>
-                <Link href={item.link} target="_blank">
-                    <button className="bg-primary text-white px-6 py-2 rounded-full mt-2 text-sm cursor-pointer">View Project</button>
-                </Link>
+              <h3 className="text-md font-bold text-primary">
+                {item.category}
+              </h3>
+              <p className="text-white font-bold text-xl">{item.title}</p>
+              <Link href={item.link} target="_blank">
+                <button className="bg-primary text-white px-6 py-2 rounded-full mt-2 text-sm cursor-pointer">
+                  View Project
+                </button>
+              </Link>
             </div>
           </div>
         ))}
